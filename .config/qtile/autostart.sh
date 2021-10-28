34df590a83fcfd198ca19c580e3eda4e50e0bb07
Ikramullah <ikramullahzx@gmail.com>
2021-10-28 09:59:35 +0800

    add: Qtile

---
+++ .config/qtile/autostart.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+thunar --daemon &
+feh --bg-scale ~/.wallpaper/default.jpg &
+dunst -conf ~/.config/dunst/dunstrc-dark &
+xfce4-power-manager &
+start-pulseaudio-x11 &
+xfsettingsd &
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+/usr/lib/xfce4/xfconf/xfconfd &
+nm-applet &
---
+++ .config/qtile/config.py
@@ -0,0 +1,173 @@
+from typing import List  # noqa: F401
+
+import os
+import subprocess
+import getpass
+from libqtile import bar, layout, widget, hook
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+
+def get_user_path(path):
+    return "/home/" + getpass.getuser() + "/" + path
+
+"""
+List shortcut keyboard
+"""
+mod = "mod4"
+keys = [
+    # Switch between windows
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key(["mod1"], "Tab", lazy.layout.next(),
+        desc="Move window focus to other window"),
+    Key([mod], "space", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+
+    # Move windows between left/right columns or move up/down in current stack.
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
+        desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(),
+        desc="Move window up"),
+
+    # Manipulate windows
+    Key([mod, "control"], "h", lazy.layout.grow_left(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(),
+        desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(),
+        desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+    Key([mod, "control"], "n", lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack"),
+
+    # Launch application
+    Key([mod], "Return", lazy.spawn("alacritty"), desc="Launch terminal"),
+    Key([mod], "r",
+        lazy.spawn(get_user_path(".config/rofi/launchers/text/launcher.sh"))),
+    Key([mod, "control"], "Return", lazy.spawn("slock")),
+
+    # Media Hotkeys
+    Key([], 'XF86AudioRaiseVolume', lazy.spawn('pamixer -i 10')),
+    Key([], 'XF86AudioLowerVolume', lazy.spawn('pamixer -d 10')),
+    Key([], 'XF86AudioMute', lazy.spawn('pulseaudio-ctl set 1')),
+
+    # Utility
+    Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
+    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod, "shift"], "r", lazy.spawncmd(),
+        desc="Spawn a command using a prompt widget"),
+]
+
+"""
+Workspace/Group
+"""
+# You can change with name that you want
+group_names = '1 2 3 4 5 6 7 8 9'.split()
+groups = [Group(name, layout='columns') for name in group_names]
+for i, name in enumerate(group_names):
+    indx = str(i + 1)
+    keys += [
+        Key([mod], indx, lazy.group[name].toscreen()),
+        Key([mod, 'shift'], indx, lazy.window.togroup(name, switch_group=True))
+    ]
+
+"""
+Layouting
+"""
+layouts = [
+    layout.Columns(
+        border_focus_stack=['#d75f5f', '#8f3d3d'], border_width=1,
+        margin=10
+    ),
+    layout.Max(),
+    layout.TreeTab(),
+]
+
+"""
+Widget and Screen
+"""
+screen_show = True
+widget_defaults = dict(
+    font='sans',
+    fontsize=12,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
+screen_bottom = bar.Bar(
+    [
+        widget.CurrentLayout(),
+        widget.GroupBox(),
+        widget.Prompt(),
+        widget.WindowName(),
+        widget.Chord(
+            chords_colors={
+                'launch': ("#ff0000", "#ffffff"),
+            },
+            name_transform=lambda name: name.upper(),
+        ),
+        widget.Volume(),
+        widget.Systray(),
+        widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+        widget.QuickExit(),
+    ],
+    24,
+)
+screens = [
+    Screen(bottom=screen_bottom),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+
+@hook.subscribe.startup_once
+def autostart():
+    """
+    Autostart
+    """
+    home = os.path.expanduser("~/.config/qtile/autostart.sh")
+    subprocess.call([home])
+
+
+@hook.subscribe.startup
+def startup():
+    screen_bottom.show(screen_show)
+
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(float_rules=[
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    *layout.Floating.default_float_rules,
+    Match(wm_class='confirmreset'),  # gitk
+    Match(wm_class='makebranch'),  # gitk
+    Match(wm_class='maketag'),  # gitk
+    Match(wm_class='ssh-askpass'),  # ssh-askpass
+    Match(title='branchdialog'),  # gitk
+    Match(title='pinentry'),  # GPG key password entry
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+wmname = "LG3D"
