d7f4c738b090660c711ea6ddaf9180f586429277
Ikramullah <ikramullahzx@gmail.com>
2022-07-20 12:31:12 +0800

    fix(gpg & awesome): more minimalize

--- .config/awesome/main/autostart.lua
+++ .config/awesome/main/autostart.lua
@@ -7,18 +7,18 @@ local vars = RC.vars
 local cmds =
 {
     -- Reinit
-    "killall tint2",
-    "killall thunar",
+    --"killall tint2",
+    --"killall thunar",
 
     -- Spawner
     "feh --bg-scale \"" .. vars.wallpaper .. "\"",
-    "thunar --daemon",
-    "xfce4-power-manager &",
-    "start-pulseaudio-x11 &",
-    "xfsettingsd &",
-    "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &",
-    "/usr/lib/xfce4/xfconf/xfconfd &",
-    "nm-applet &",
+    --"thunar --daemon",
+    --"xfce4-power-manager &",
+    --"start-pulseaudio-x11 &",
+    --"xfsettingsd &",
+    --"/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &",
+    --"/usr/lib/xfce4/xfconf/xfconfd &",
+    --"nm-applet &",
 
     -- Shell
     RC.vars.statusbar and '' or "bash -c ~/.config/awesome/shell/spawn_tint2.sh"
