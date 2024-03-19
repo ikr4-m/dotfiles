733d1839f6f0863d316ee79ebb9cf3ec5c1dc8c8
Ikramullah <ikramullahzx@gmail.com>
2024-03-19 20:56:03 +0800

    nvim: change coc to lspconfig+cmp+trouble

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -107,58 +107,6 @@ require("lazy").setup({
     end
   },
   {
-    "neoclide/coc.nvim",
-    branch = "release",
-    init = function()
-      local keyset = vim.keymap.set
-      vim.cmd([[ 
-        " Open coc diagnostic
-        nnoremap <Leader>d :CocCommand diagnostic<CR>
-
-        " Diagnostic
-        nmap <silent> [g <Plug>(coc-diagnostic-prev)
-        nmap <silent> ]g <Plug>(coc-diagnostic-next)
-      
-        " Definition overload
-        nmap <silent> gd <Plug>(coc-definition)
-        nmap <silent> gy <Plug>(coc-type-definition)
-        nmap <silent> gi <Plug>(coc-implementation)
-        nmap <silent> gr <Plug>(coc-references)
-      ]])
-
-      -- Use Tab for trigger completion with characters ahead and navigate
-      -- NOTE: There's always a completion item selected by default, you may want to enable
-      -- no select by setting `"suggest.noselect": true` in your configuration file
-      -- NOTE: Use command ':verbose imap <tab>' to make sure Tab is not mapped by
-      -- other plugins before putting this into your config
-      local opts = {silent = true, noremap = true, expr = true, replace_keycodes = false}
-      keyset("i", "<TAB>", 'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', opts)
-      keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
-      
-      -- Make <CR> to accept selected completion item or notify coc.nvim to format
-      -- <C-g>u breaks current undo, please make your own choice
-      keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
-      
-      -- Use <c-j> to trigger snippets
-      keyset("i", "<c-j>", "<Plug>(coc-snippets-expand-jump)")
-      -- Use <c-space> to trigger completion
-      keyset("i", "<c-space>", "coc#refresh()", {silent = true, expr = true})
-      
-      -- Use K to show documentation in preview window
-      function _G.show_docs()
-          local cw = vim.fn.expand('<cword>')
-          if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
-              vim.api.nvim_command('h ' .. cw)
-          elseif vim.api.nvim_eval('coc#rpc#ready()') then
-              vim.fn.CocActionAsync('doHover')
-          else
-              vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
-          end
-      end
-      keyset("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
-    end,
-  },
-  {
     "lewis6991/gitsigns.nvim",
     config = function()
       require('gitsigns').setup()
@@ -223,13 +171,118 @@ require("lazy").setup({
   --------------------------
   -- LSP
   --------------------------
-  --"dense-analysis/ale",
-  "elkowar/yuck.vim",
+  "folke/trouble.nvim",
   {
-    "OmniSharp/omnisharp-vim",
-    init = function()
-      vim.cmd("autocmd FileType cs nnoremap <silent> K :OmniSharpDocumentation<CR>")
-      vim.cmd("let g:OmniSharp_server_use_net6 = 1")
+    "elkowar/yuck.vim",
+    event = "VeryLazy",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    event = "VeryLazy",
+    init = function ()
+      local lspconfig = require('lspconfig')
+      lspconfig.tsserver.setup({})
+      lspconfig.eslint.setup({})
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    event = "VeryLazy",
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/vim-vsnip',
+      'hrsh7th/cmp-nvim-lsp-signature-help'
+    },
+    config = function ()
+      local cmp = require('cmp')
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            vim.fn["vsnip#anonymous"](args.body)
+          end,
+        },
+        window = {
+          --completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif vim.fn["vsnip#available"](1) == 1 then
+              feedkey("<Plug>(vsnip-expand-or-jump)", "")
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
+            end
+          end, { "i", "s" }),
+          ["<S-Tab>"] = cmp.mapping(function()
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+              feedkey("<Plug>(vsnip-jump-prev)", "")
+            end
+          end, { "i", "s" }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'vsnip' },
+          { name = 'nvim_lsp_signature_help' },
+        }, {
+          { name = 'buffer' },
+        })
+      })
+      -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline({ '/', '?' }, {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+      -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        })
+      })
+    end,
+  },
+  {
+    "lewis6991/hover.nvim",
+    event = "VeryLazy",
+    config = function ()
+      require('hover').setup({
+        init = function()
+          require("hover.providers.lsp")
+        end,
+        preview_opts = {
+          border = 'single'
+        },
+        preview_window = false,
+        title = true,
+        mouse_providers = {
+            'LSP'
+        },
+        mouse_delay = 1000
+      })
+      -- Setup keymaps
+      vim.keymap.set("n", "K", require("hover").hover, {desc = "hover.nvim"})
+      vim.keymap.set("n", "gK", require("hover").hover_select, {desc = "hover.nvim (select)"})
+      vim.keymap.set("n", "<C-p>", function() require("hover").hover_switch("previous") end, {desc = "hover.nvim (previous source)"})
+      vim.keymap.set("n", "<C-n>", function() require("hover").hover_switch("next") end, {desc = "hover.nvim (next source)"})
     end,
   },
 
