33b00ef91d6fa646b6f39a1f528338079d131590
Ikramullah <ikramullahzx@gmail.com>
2024-03-21 00:42:19 +0800

    nvim: add neocord

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -23,6 +23,24 @@ require("lazy").setup({
   --------------------------
   "Yggdroot/indentLine",
   {
+    'IogaMaster/neocord',
+    event = "VeryLazy",
+    config = function ()
+      require("neocord").setup({
+        show_time = true,
+        global_timer = false,
+        editing_text = "Editing %s",
+        file_explorer_text = "Browsing %s",
+        git_commit_text = "Committing changes",
+        plugin_manager_text = "Managing plugins",
+        reading_text = "Reading %s",
+        workspace_text = "Working on %s",
+        line_number_text = "Line %s out of %s",
+        terminal_text = "Using Terminal",
+      })
+    end,
+  },
+  {
     "nvim-lualine/lualine.nvim",
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     init = function()
@@ -186,6 +204,8 @@ require("lazy").setup({
     event = "VeryLazy",
     init = function ()
       local lspconfig = require('lspconfig')
+
+      -- JavaScript
       lspconfig.tsserver.setup({})
       lspconfig.eslint.setup({})
     end,
